3c2b2766d5e8d21cfab9c0700f7fc276daf664ce
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2023-06-22 15:35:19 +0200

    Update link

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href="/test/[id]" as="/test/1">
+      <Link href="/test/[id]?id=1" as="/test/1">
         First
       </Link>
       <Link href="/test/2">Seconds</Link>
