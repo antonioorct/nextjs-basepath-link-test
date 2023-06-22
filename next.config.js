e1c68f986c08a754e99adbb36455b56c5041ee32
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2023-06-22 10:46:31 +0200

    Remove redirect

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/basepath",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/basepath",
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
