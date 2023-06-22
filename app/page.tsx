2bbd8553129dcc1c1bf1e1d7373a3214b9775990
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2023-06-22 15:45:42 +0200

    Add test button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const test = () => {
+    router.prefetch("/test/4");
+  };
+
   return (
     <>
+      <button onClick={test}></button>
       <Link href="/test/[id]?id=1" as="/test/1">
         First
       </Link>
