dc9f808f1b163ecc84c8a3c3a89694e915b6c009
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2023-06-22 16:08:07 +0200

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Link href="/test/[id]?id=1" as="/test/1">
         First
       </Link>
-      <Link href="/test/2">Seconds</Link>
+      <Link href="/test/2">Second</Link>
       <Link href="/test/[id]" as="/test/3">
         Third
       </Link>
