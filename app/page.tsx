b022c2900537d5dfdfd3f440a28d1ec83035d7a1
Antonio Orct <14859262+antonioorct@users.noreply.github.com>
2023-06-22 11:07:46 +0200

    Remove cache

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href="/test/1">First</Link>
+      <Link href="/test/[id]" as="/test/1">
+        First
+      </Link>
       <Link href="/test/2">Seconds</Link>
     </>
   );
--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,6 +1,9 @@
+export const revalidate = 0;
+
 type Props = {
   params: { id: string };
 };
+
 export default async function Page({ params: { id } }: Props) {
   return <>{id}</>;
 }
